7f99613ed9543d0faa73e7c127727e05e7c8746e
petri.sarasvirta <petri.sarasvirta@gofore.com>
2017-11-24 17:41:42 +0200

    battlebot

--- startBattle.ps1
+++ startBattle.ps1
@@ -2,3 +2,4 @@ start-process node -ArgumentList '.\example_bots\dumb-bot.js', 4001
 start-process node -ArgumentList '.\example_bots\dumb-bot.js', 4002
 start-process node -ArgumentList '.\example_bots\dumb-bot.js', 4003
 start-process node -ArgumentList '.\example_bots\dumb-bot.js', 4004
+start-process nmp -ArgumentList 'run', 'start:all'
