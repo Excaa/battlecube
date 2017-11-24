63d3f183e1be039b4d596f7312034bf6186edae8
petri.sarasvirta <petri.sarasvirta@gofore.com>
2017-11-24 17:34:01 +0200

    new startbattlescript

---
+++ startBattle.ps1
@@ -0,0 +1,4 @@
+start-process node -ArgumentList '.\example_bots\dumb-bot.js', 4001
+start-process node -ArgumentList '.\example_bots\dumb-bot.js', 4002
+start-process node -ArgumentList '.\example_bots\dumb-bot.js', 4003
+start-process node -ArgumentList '.\example_bots\dumb-bot.js', 4004
